b1d27e965614c8ab8a9c9b12a72a562039b1dbe2
GustavoRoss20 <gustavoros20@outlook.com>
2025-01-15 22:03:02 -0600

    Se completa el challenge

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+const friendsNames = [];
+
+function agregarAmigo() {
+  const inputDom = document.getElementById("amigo");
+  const nameFriend = inputDom.value.trim();
+
+  if (!nameFriend) {
+    alert("Por favor, inserte un nombre.");
+    return;
+  }
+
+  friendsNames.push(nameFriend);
+  inputDom.value = "";
+  actualizarListaAmigos();
+}
+
+function actualizarListaAmigos() {
+  const lista = document.getElementById("listaAmigos");
+  lista.innerHTML = "";
+
+  friendsNames.forEach((friend) => {
+    const item = document.createElement("li");
+    item.textContent = friend;
+    lista.appendChild(item);
+  });
+}
+
+function sortearAmigo() {
+  if (friendsNames.length === 0) {
+    alert("La lista de amigos está vacía. Agrega al menos un amigo antes de sortear.");
+    return;
+  }
+
+  const randomIndex = Math.floor(Math.random() * friendsNames.length);
+  const listaResult = document.getElementById("resultado");
+  listaResult.innerHTML = "";
+
+  const winnerElement = document.createElement("li");
+  winnerElement.textContent = `El amigo sorteado es: ${friendsNames[randomIndex]}`;
+  listaResult.appendChild(winnerElement);
+}
